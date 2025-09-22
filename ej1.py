cdb6cbaca46332abb8f6c955d44a595a8905a13e
franns008 <franns008@gmail.com>
2025-09-22 15:50:06 -0300

    subo practcia

--- ej1.py
+++ ej1.py
@@ -3,35 +3,31 @@ from pwn import *
 # context.log_level = 'debug'
 # Analice las diferencias entre usar o no el debug
 # Nos conectamos utilizando remote
-con = remote("163.10.20.26", 10002)
+con = remote("ic.catedras.linti.unlp.edu.ar", 10001)
 # para quitar el texto que no nos interesa (banner),
 # leemos hasta justo antes de la cuenta, es decir, hasta ":\n"
-
-con.recvuntil(b"flag!:\n") 
-
+con.readuntil("resolver esta cuenta:\n")
 # Leemos hasta el salto de línea, la cuenta deseada
-cuenta = con.recvline(timeout=0.2)
+cuenta = con.readline()
 print(type(cuenta))
 print(cuenta)
 # Pasamos los bytes a string, para poder realizar la cuenta
-cuenta = cuenta.decode().split()
-
+cuenta = cuenta.decode()
+# Split convierte una cadena de texto en una lista, utilizando como separador los
+# espacios en blanco
+cuenta = cuenta.split() # ['297', '+', '155']
 # Convierto a entero los operandos
 op1 = int(cuenta[0])
 op2 = int(cuenta[2])
-
-# El operador es el segundo elemento de la lista
 operador = cuenta[1]
 # Sumo multiplico o resto según el operador
-if operador == '-':
- resultado = op1 - op2
+if operador == '+':
+ resultado = op1 + op2
 elif operador == '*':
  resultado = op1 * op2
 else:
-    resultado = op1 + op2
+ resultado = op1 - op2
 # Enviamos la respuesta de la cuenta, como bytes:
-con.sendline((str(resultado) + "\n").encode())
+con.send((str(resultado) + "\n").encode())
 # Imprimimos toda la respuesta del servidor
-print(con.recvall(timeout=0.3).decode())
-
-#CONSULTAR, PORQUE AHORA ESTO ANDA RAPIDO? A DIFERENCIA DEL DE ANTES
+print(con.readall())
